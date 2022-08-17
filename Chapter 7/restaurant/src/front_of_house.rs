d164332cebd42a8a7918ef683bbac5d8401dff0d
deimoshall <deimoshall@gmail.com>
2022-08-17 09:14:34 -0500

    Separating modules into different files

---
+++ Chapter 7/restaurant/src/front_of_house.rs
@@ -0,0 +1,10 @@
+// hosting and add_to_waitlist() need to be public to be accesded
+pub mod hosting;
+
+mod serving {
+    fn take_order() {}
+
+    fn serve_order() {}
+
+    fn take_payment() {}
+}
--- Chapter 7/restaurant/src/lib.rs
+++ Chapter 7/restaurant/src/lib.rs
@@ -1,22 +1,7 @@
 // Modules can also hold definitions for other items, such as structs,
 // enums, constants, traits or functions
 #[allow(dead_code)]
-mod front_of_house {
-    // hosting and add_to_waitlist() need to be public to be accesded
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 // Creating a shortchut with 'use'
 use crate::front_of_house::hosting;
