a6561da98800b043d7ada35fa71d51425a42ab4d
deimoshall <deimoshall@gmail.com>
2022-08-17 08:53:03 -0500

    'use' keyword section added

--- Chapter 7/restaurant/src/lib.rs
+++ Chapter 7/restaurant/src/lib.rs
@@ -18,10 +18,24 @@ mod front_of_house {
     }
 }
 
+// Creating a shortchut with 'use'
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Path with shortcut
+    hosting::add_to_waitlist();
+}
+
+#[allow(dead_code)]
+mod customer {
+    pub fn eat_at_restaurant() {
+        // supper access to a top level in the tree of filesystem
+        super::hosting::add_to_waitlist();
+    }
 }
