571f78d9479ea0f3913958cc322df95441e1c29a
Deimos <deimoshall@gmail.com>
2022-09-05 19:54:33 -0500

    Working on reading a file

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -2,6 +2,14 @@ use std::env;
 // std::env::args_os can receive arguments with invalid Unicode
 
 fn main() {
+    // collect() converts an iterator into a vector
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    //dbg!(args);
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
---
+++ Chapter 12/minigrep/target/debug/deps/minigrep-a4e91e1290634b34.d
@@ -0,0 +1,5 @@
+/home/deimos/Software Dev/Rust/Basics-of-Rust/Chapter 12/minigrep/target/debug/deps/minigrep-a4e91e1290634b34: src/main.rs
+
+/home/deimos/Software Dev/Rust/Basics-of-Rust/Chapter 12/minigrep/target/debug/deps/minigrep-a4e91e1290634b34.d: src/main.rs
+
+src/main.rs:
