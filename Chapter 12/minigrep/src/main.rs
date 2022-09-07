6e8038334da3b4d43be40cc17bb87f8d942171b3
deimos <deimoshall@gmail.com>
2022-09-07 15:31:03 -0500

    Ignore

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -1,7 +1,13 @@
 use std::env;
 // std::env::args_os can receive arguments with invalid Unicode
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    //dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("{}, {}", query, file_path);
 }
