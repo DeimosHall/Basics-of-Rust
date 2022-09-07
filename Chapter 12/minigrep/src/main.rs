792307044547aec91572915792dc5a2f3a9d3a0c
deimos <deimoshall@gmail.com>
2022-09-07 15:45:12 -0500

    Refactoring and error handling

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -1,24 +1,22 @@
 use std::env;
 // std::env::args_os can receive arguments with invalid Unicode
+// fs is to manage file system
 use std::fs;
 
 fn main() {
     // collect() converts an iterator into a vector
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
-<<<<<<< HEAD
+    println!("{:?}", args);
 
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("{}, {}", query, file_path);
-=======
-    println!("{:?}", args);
-
-    let query = &args[1];
-    let filename = &args[2];
-
     println!("Searching for {}", query);
-    println!("In file {}", filename);
->>>>>>> 571f78d9479ea0f3913958cc322df95441e1c29a
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    
+    println!("With text: \n{}", contents);
 }
--- Chapter 12/minigrep/target/debug/minigrep.d
+++ Chapter 12/minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/deimos/Software\ Dev/Rust/Basics-of-Rust/Chapter\ 12/minigrep/target/debug/minigrep: /home/deimos/Software\ Dev/Rust/Basics-of-Rust/Chapter\ 12/minigrep/src/main.rs
+/home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter\ 12/minigrep/target/debug/minigrep: /home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter\ 12/minigrep/src/main.rs
