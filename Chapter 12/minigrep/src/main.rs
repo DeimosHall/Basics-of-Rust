f99a7564058987f0f8c2276b521246da580e8771
deimos <deimoshall@gmail.com>
2022-09-13 17:25:26 -0500

    Adding a user friendly error message when there are not arguments

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 // std::env::args_os can receive arguments with invalid Unicode
 // fs is to manage file system
 use std::fs;
+use std::process;
 
 fn main() {
     // collect() converts an iterator into a vector
@@ -9,7 +10,10 @@ fn main() {
     //dbg!(args);
     //println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -26,6 +30,7 @@ struct Config {
 }
 
 impl Config {
+    /*
     fn new(args: &[String]) -> Config {
         if args.len() < 3 {
             panic!("not enough arguments");
@@ -35,5 +40,16 @@ impl Config {
         let file_path = args[2].clone();
 
         Config { query, file_path }
+    } */
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
     }
 }
