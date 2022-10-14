f277fdf1c6593e248f313b20e091ea3f77edf43d
deimoshall <deimoshall@gmail.com>
2022-10-14 12:52:39 -0500

    Add git ignore file

---
+++ Chapter 13/closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter\ 13/closures/target/debug/closures: /home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter\ 13/closures/src/main.rs
---
+++ Chapter 13/closures/target/debug/deps/closures-cbaddbd7308884c1.d
@@ -0,0 +1,5 @@
+/home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter 13/closures/target/debug/deps/closures-cbaddbd7308884c1: src/main.rs
+
+/home/deimos/Software-dev/Rust/Basics-of-Rust/Chapter 13/closures/target/debug/deps/closures-cbaddbd7308884c1.d: src/main.rs
+
+src/main.rs:
