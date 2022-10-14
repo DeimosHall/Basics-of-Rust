df52c458d07286df250d71f282f7a0e040dbe612
deimoshall <deimoshall@gmail.com>
2022-10-14 12:31:05 -0500

    Print errors to standard error output

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -16,8 +16,8 @@ fn main() {
      * exected.
     */
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        println!("Expected: word file.txt");
+        eprintln!("Problem parsing arguments: {}", err);
+        eprintln!("Expected: word file.txt");
         // A nonzero value means the program finishes with a problem
         process::exit(1);
     });
@@ -29,7 +29,7 @@ fn main() {
      * care if there was a problem.
      */
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
