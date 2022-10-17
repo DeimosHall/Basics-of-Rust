c24e1b31e0a2f7e3c0f488a1d0f4f84cabf0da44
Deimos <deimoshall@gmail.com>
2022-10-16 21:53:08 -0500

    Closure type inference and annotation

--- Chapter 13/closures/src/main.rs
+++ Chapter 13/closures/src/main.rs
@@ -1,3 +1,77 @@
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!("The user with preference {:?} gets {:?}", user_pref1, giveaway1);
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
+
+    // Closures can be defined in a verbose way, and can be store in variables
+    let expensive_closure = |num: u32| -> u32 {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    // Closures can be written in a short way, like in these examples.
+    fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let add_one_v3 = |x| { x + 1 };
+    let add_one_v4 = |x| x + 1;
+
+    // In versions 3 and 4 Rust needs have a call to the closure to be able
+    // to infer the parameter type.
+    let add_example1 = add_one_v3(5);
+    let add_example2= add_one_v4(3);
+
+    // The first time we use a closure, Rust locks the parameter type
+    // and the next time we want to use it, it will give us an error
+    // in the commented n variable.
+    let example_closure = |x| x;
+
+    let s = example_closure("Hello");
+    //let n = example_closure(5);
+
+    
 }
