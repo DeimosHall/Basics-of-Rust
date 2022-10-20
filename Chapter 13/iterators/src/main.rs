c6de410f468c20f955a63a2902de3630beaf6245
deimoshall <deimoshall@gmail.com>
2022-10-20 17:02:21 -0500

    Methods that produce other iterators

--- Chapter 13/iterators/src/main.rs
+++ Chapter 13/iterators/src/main.rs
@@ -6,4 +6,14 @@ fn main() {
     for item in v1_iter {
         println!("{}", item);
     }
+
+    // map is an iterator that produces other itarator
+    // it also needs to be consume
+    v1.iter().map(|x| x + 1);
+
+    // We can conume the iterator with the method collect,
+    // that returns the values into a collection data type
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    println!("{:#?}", v2);
 }
