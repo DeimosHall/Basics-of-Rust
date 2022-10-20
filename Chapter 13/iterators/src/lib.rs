9fdd2b3d9994ac11ffc2182d07a89f2ef9e9a4aa
deimoshall <deimoshall@gmail.com>
2022-10-20 17:18:00 -0500

    Using closures that capture their enviroment

--- Chapter 13/iterators/src/lib.rs
+++ Chapter 13/iterators/src/lib.rs
@@ -1,28 +1,78 @@
-#[test]
-fn iterator_demonstration() {
-    let v1 = [1, 2, 3];
-    let mut v1_iter = v1.iter();
-
-    // iter() returns immutable references of the items.
-    // into_iter() creates an iterator that takes ownership
-    // of the variable.
-    // iter_mut() itarates over mutable references.
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    // When the itarator do not find an item,
-    // it returns a None value.
-    assert_eq!(v1_iter.next(), None);
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
 
-#[test]
-fn iterator_sum() {
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    // into_iter takes ownership of the vector
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
 
-    // There are method like sum() that
-    // takes ownership of the iterator.
-    let total: i32 = v1_iter.sum();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    assert_eq!(total, 6);
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = [1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        // iter() returns immutable references of the items.
+        // into_iter() creates an iterator that takes ownership
+        // of the variable.
+        // iter_mut() itarates over mutable references.
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        // When the itarator do not find an item,
+        // it returns a None value.
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+
+        // There are method like sum() that
+        // takes ownership of the iterator.
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneacker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneacker"),
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot"),
+                },
+            ]
+        );
+    }
 }
