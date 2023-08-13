28a43205cb62c18e096e81f3935ed530237fd164
deimos <deimoshall@gmail.com>
2023-08-13 14:17:10 -0600

    feat: add variables

---
+++ chapter-3/variables/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+    // x = 6; // error[E0384]: cannot assign twice to immutable variable `x`
+    println!("The value of x is: {}", x);
+
+    let mut y = 5;
+    println!("The value of y is: {}", y);
+    y = 6;
+    println!("The value of y is: {}", y);
+
+    // Constants
+    const DEAY_HOURS: u32 = 24;
+    println!("The value of DEAY_HOURS is: {}", DEAY_HOURS);
+
+    // Shadowing
+    let z = 5;
+    let z = z + 1;
+
+    {
+        let z = z * 2;
+        println!("The value of z is: {}", z); // 12
+    }
+    println!("The value of z is: {}", z); // 6
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces); // 3
+}
