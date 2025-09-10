eae79973ef42015b0d17a018d22d60153cb8b884
deimos <deimoshall@gmail.com>
2025-09-09 20:07:42 -0600

    feat(chapter-7): add restaurant lib crate

---
+++ chapter-7/restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_wait_list() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
