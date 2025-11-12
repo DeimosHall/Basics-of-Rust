44adb8e1c05694b50aa8186057df658e7c3edec9
deimos <deimoshall@gmail.com>
2025-11-11 19:54:10 -0600

    feat(chapter-7): use of paths and pub keyword

--- chapter-7/restaurant/src/lib.rs
+++ chapter-7/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_wait_list() {}
+    pub mod hosting {
+        pub fn add_to_wait_list() {}
         fn seat_at_table() {}
     }
 
@@ -9,4 +9,59 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    // We have to define one by one the public fields
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String, // private
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // But in enums all fields al public if the enum is public
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        self::cook_order();
+        // Use functions from the parent
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_retaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_wait_list();
+
+    // Relative path
+    front_of_house::hosting::add_to_wait_list();
+
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // If enum is public, fields are public
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
