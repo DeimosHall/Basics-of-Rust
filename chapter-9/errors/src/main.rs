5bacf48acf6dba658a43a78979d052b6b79a0d46
deimos <deimoshall@gmail.com>
2025-11-24 20:33:36 -0600

    feat(chapter-9): add panic examples

---
+++ chapter-9/errors/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    // Intentional panic
+    // panic!("Crashing the program");
+    
+    // Undesired panic
+    let vector = vec![1, 2, 3];
+    // let _ = vector[4];  // Uncomment
+}
