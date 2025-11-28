14c87498fe184071e3690b884823b16a4ebafe15
deimos <deimoshall@gmail.com>
2025-11-27 20:38:22 -0600

    feat(chapter-9): implement guess type for error handling

--- chapter-2/guessing_game/src/main.rs
+++ chapter-2/guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: u8,
+}
+
+// Only returns the type if the condition is met
+impl Guess {
+    pub fn new(value: u8) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u8 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number between 1 and 100!");
 
@@ -20,18 +38,20 @@ fn main() {
     
         // trim spaces and parse to u8, it is inferred from the variable type
         // let guess: u8 = guess.trim().parse().expect("Please type a positive number!");
-        let guess: u8 = match guess.trim().parse() {
-            Ok(number) => number,
-            Err(_) => {
-                println!("'{}' is not a positive number, please type one!", guess.trim());
-                continue;
-            },
-        };
+        // let guess: u8 = match guess.trim().parse() {
+        //     Ok(number) => number,
+        //     Err(_) => {
+        //         println!("'{}' is not a valid number, please type one!", guess.trim());
+        //         continue;
+        //     },
+        // };
+        
+        let guess: Guess = Guess::new(guess.trim().parse().expect("Please type a valid number"));
     
-        println!("You guessed: '{guess}'");
+        println!("You guessed: '{}'", guess.value());
     
         // Compare guess and secret number
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
