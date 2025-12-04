d0d4f90a9de56907fcc760c63849f1dff1816f2f
deimos <deimoshall@gmail.com>
2025-12-03 21:18:49 -0600

    feat(chapter-10): add generics structs

--- chapter-10/generics/src/generics.rs
+++ chapter-10/generics/src/generics.rs
@@ -9,3 +9,28 @@ pub fn find_largest<T: PartialOrd>(list: &[T]) -> &T {
     
     largest
 }
+
+// We can also define generics structs
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
+}
+
+pub mod Another {
+    pub struct Point<T, U> {
+        pub x: T,
+        pub y: U,
+    }
+    
+    impl<T, U> Point<T, U> {
+        pub fn x(&self) -> &T {
+            &self.x
+        }
+    }
+    
+    impl Point<f32, f32> {
+        pub fn hello(&self) -> String {
+            "hello".to_string()
+        }
+    }
+}
--- chapter-10/generics/src/main.rs
+++ chapter-10/generics/src/main.rs
@@ -34,4 +34,29 @@ fn main() {
     println!("With generics:");
     println!("The largest number is {}", generics::find_largest(&i32_list));
     println!("The largest char is {}", generics::find_largest(&char_list));
+    
+    println!("Using generic structs");
+    let integer = generics::Point { x: 5, y: 10 };
+    println!("x: {}, y: {}", integer.x, integer.y);
+    let float = generics::Point { x: 5.1, y: 10.3 };
+    println!("x: {}, y: {}", float.x, float.y);
+    
+    // We can't create a generics point with different
+    // data types because they share the same T type
+    // let wont_work = Point { x: 5, y: 4.0 };
+    
+    // To acheive the previous behavior, we can use a Point<T, U>
+    let will_work = generics::Another::Point { x: 5, y: 4.0 };
+    println!("x: {}, y: {}", will_work.x, will_work.y);
+    
+    // We can also implement functions to generic data types, for example
+    // a getter to a Point field
+    println!("x value: {}", will_work.x());
+    
+    // We can have specific behavior for specific types, for example, a method
+    // that returns hello only on Point<f32, f32> types
+    let will_work = generics::Another::Point { x: 1.2, y: 2.1 };
+    println!("Say {}", will_work.hello());
+    let wont_work = generics::Another::Point { x: 1.5, y: 2 };
+    // println!("Say {}", wont_work.hello());
 }
