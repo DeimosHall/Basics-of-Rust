1c120f351d7599ae62caac32282ef4c2a3e2da7a
deimos <deimoshall@gmail.com>
2025-12-18 19:46:03 -0600

    feat(chapter-11): add how to write tests

---
+++ chapter-11/adder/src/lib.rs
@@ -0,0 +1,66 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn it_does_not_work() {
+        let result = add(3, 2);
+        let expected = 4;
+        assert_eq!(4, result, "expected: {}, actual: {}", expected, result);
+    }
+    
+    #[test]
+    fn test_that_fails() {
+        panic!("This test fails");
+    }
+    
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+    
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
