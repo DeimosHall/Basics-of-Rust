0072f34ac5a14e0a103428ced3408ee4829a76b5
deimos <deimoshall.dev@proton.me>
2026-01-19 09:11:18 -0600

    feat(chapter-14): add workspaces example

---
+++ chapter-14/add/add_one/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add_one(x: u64) -> u64 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(2);
+        assert_eq!(result, 3);
+    }
+}
---
+++ chapter-14/add/adder/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let num = 5;
+    println!("{} plus one is: {}", num, add_one::add_one(num));
+}
